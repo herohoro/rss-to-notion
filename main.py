cdcd5e36628252c6f1bf43b17c107a5cd0476a78
herohoro <mtwd.office.25@gmail.com>
2023-06-24 02:10:49 +0900

    dalate commentOut_byCopilot

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import datetime
 def addContent(title, date, link):
     
     token = os.getenv('TOKEN')
-    # I want to use databaseId from .env file
-    # databaseId = os.getenv('DATABASE_ID')
     databaseId = os.getenv('DATABASE_ID')
 
     headers = {
@@ -54,20 +52,6 @@ def job(url):
     for entry in elements.entries:
         title = entry.title
         date = entry.published
-        # sample is Fri, 23 Jun 2023 11:45:00 GMT
-        # but I want Mon, 27 Mar 2023 00:00:00 +0000
-
-        # strptimeで文字列をdatetime型に変換
-        # strptime(文字列, フォーマット) → strftime(フォーマット)
-        # フォーマット → https://docs.python.org/ja/3/library/datetime.html#strftime-and-strptime-format-codes
-        # Mon is format code %a
-        # day is format code %d
-        # %Z is timezone
-        # +0000 is timezone
-        # What difference between %Z and +0000?
-        # %Z is timezone name
-        # +0000 is timezone offset
-        
         date = datetime.datetime.strptime(str(date), '%a, %d %b %Y %H:%M:%S +0000').strftime('%Y-%m-%d %H:%M')
         link  = entry.link       
         addContent(title, date, link)
